310f0c2537a69d7b8689448a63bfe5c669e71c1b
Nantim Lord <nantimlord@outlook.com>
2024-01-19 19:43:18 +0100

    client deployment

--- frontend/src/api/index.jsx
+++ frontend/src/api/index.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "http://127.0.0.1:5000"
+const baseUrl = "https://tasker-seven-beryl.vercel.app"
 const api = axios.create({
   baseURL: `${baseUrl}/api`,
 });
